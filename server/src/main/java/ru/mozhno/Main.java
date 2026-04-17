9b4362f439d3683dddd7eda1a2b216b1be363427
edgar.gilmanov <edgar.gilmanov@megafon.ru>
2026-04-17 19:15:48 +0300

    first commit

---
+++ server/src/main/java/ru/mozhno/Main.java
@@ -0,0 +1,14 @@
+package ru.mozhno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class Main {
+
+    static void main(String[] args) {
+        SpringApplication.run(Main.class);
+    }
+
+}
